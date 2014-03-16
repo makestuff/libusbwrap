d4ac4923b4504f64dbbf93404f65110c8a8b4bed
Chris McClelland <prophet3636@gmail.com>
2014-03-16 15:05:10 +0000

    Add mingw support

--- deps.mk
+++ deps.mk
@@ -1,12 +1,23 @@
 ifeq ($(OS),Windows_NT)
 	LIBUSB_VERSION := 1.0.14
 	MACHINE := $(shell echo $${MACHINE})
+	PLATFORM := $(shell echo $${PLATFORM})
 	ifeq ($(MACHINE),x64)
-		LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS64/dll/libusb-1.0.lib
-		EXTRADLLS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS64/dll/libusb-1.0.dll
+		ifeq ($(PLATFORM),mingw)
+			LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MinGW64/dll/libusb-1.0.dll
+			EXTRADLLS_REL := $(LINK_EXTRALIBS_REL)
+		else
+			LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS64/dll/libusb-1.0.lib
+			EXTRADLLS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS64/dll/libusb-1.0.dll
+		endif
 	else ifeq ($(MACHINE),x86)
-		LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS32/dll/libusb-1.0.lib
-		EXTRADLLS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS32/dll/libusb-1.0.dll
+		ifeq ($(PLATFORM),mingw)
+			LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MinGW32/dll/libusb-1.0.dll
+			EXTRADLLS_REL := $(LINK_EXTRALIBS_REL)
+		else
+			LINK_EXTRALIBS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS32/dll/libusb-1.0.lib
+			EXTRADLLS_REL := $(ROOT)/3rd/libusbx-$(LIBUSB_VERSION)-win/MS32/dll/libusb-1.0.dll
+		endif
 	else ifndef MACHINE
 $(error MACHINE must be set to x86 or x64)
 	else
